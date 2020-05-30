236ed3ea440df9029c8cbd01deb1ff12a506b0f4
Juggernoaf <handfulofvoid@gmail.com>
2020-05-30 19:24:18 +0500

    Added the OC Project (C#)

---
+++ OCData/OCData/ApiClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+
+namespace OCData
+{
+    public static class ApiClient
+    {
+        private static readonly string ApiUrl = "https://api.opencorporates.com/v0.4/companies/";
+
+        private static HttpClient HttpClient { get; set; }
+
+        static ApiClient()
+        {
+            HttpClient = new HttpClient();
+        }
+
+        /// <summary>
+        /// Obtain list of companies matching name, using access token
+        /// if available
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="apiToken"></param>
+        /// <returns></returns>
+        public static string GetCompanies(string name, string apiToken = null)
+        {
+            var nameEnc = WebUtility.UrlEncode(name);
+            var url = ApiUrl + $"search?q={nameEnc}";
+            if (apiToken != null)
+            {
+                url = url + @"&api_token={apiToken}";
+            }
+            var resp = HttpClient.GetAsync(url).Result;
+
+            return resp.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
---
+++ OCData/OCData/CompanyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OCData
+{
+    public class CompanyInfo
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string JurisdictionCode { get; set; }
+        public string IncorporationDate { get; set; }
+        public string DissolutionDate { get; set; }
+        public string CompanyType { get; set; }
+        public string RegistryUrl { get; set; }
+        public string Branch { get; set; }
+
+        public string BranchStatus { get; set; }
+        public string Inactive { get; set; }
+        public string CurrentStatus { get; set; }
+        public string CreatedAt { get; set; }
+        public string RetrievedAt { get; set; }
+        public string UpdatedAt { get; set; }
+        public string OpenCorporatesUrl { get; set; }
+        public string PreviousNames { get; set; }
+        public string DataSource { get; set; }
+        public string DataSourceUrl { get; set; }
+        public string RetrievedFromSourceAt { get; set; }
+        public string RegisteredAddress { get; set; }         
+        public string StreetAddress { get; set; }
+        public string Locality { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string RegisteredAddressFull { get; set; }
+        public string IndustryCodes { get; set; }
+        public string RestrictedForMarketing { get; set; }
+        public string NativeCompanyNumber { get; set; }
+    }
+}
